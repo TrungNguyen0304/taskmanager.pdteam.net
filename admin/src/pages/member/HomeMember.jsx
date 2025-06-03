8966f09bc284145069004a3b37864b5e75803ecd
lequythien <zacubj2@gmail.com>
2025-06-03 15:15:29 +0700

    test

--- admin/src/pages/member/HomeMember.jsx
+++ admin/src/pages/member/HomeMember.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const HomeMember = () => {
   return (
     <div className="bg-white p-6 rounded-lg shadow-md">
-      <h2 className="text-2xl font-bold mb-4">Trang Chủ</h2>
+      <h2 className="text-2xl font-bold mb-4">Trang Chủ Member</h2>
       <p>Chào mừng bạn đến với hệ thống quản lý nhân sự!</p>
     </div>
   );
