8ce3e77ec8bcfa0f33d31f59bec09c94cd8a85cd
TrungNguyen0304 <TrungNguyen15989@gmail.com>
2025-06-20 16:20:30 +0700

    fix

--- admin/src/services/socketService.jsx
+++ admin/src/services/socketService.jsx
@@ -11,16 +11,20 @@ export const initializeSocket = (userId) => {
 };
 
 export const onNotification = (callback) => {
-  socket.on("team-assigned", callback);
-  socket.on("project-assigned", callback);
-  socket.on("project-removed", callback);
-  socket.on("task-assigned", callback);
-  socket.on("task-removed", callback);
-  socket.on("report-submitted", callback);
-  socket.on("report-evaluated", callback);
-  socket.on("task-overdue", callback);
-  socket.on("project-overdue", callback);
-  socket.on("project-status-updated", callback);
+  socket.on("team-assigned", callback);//1
+  socket.on("project-assigned", callback);//2
+  socket.on("project-removed", callback);//3
+  socket.on("task-assigned", callback);//4
+  socket.on("task-removed", callback);//5
+  socket.on("report-submitted-member", callback);//6
+  socket.on("report-evaluated-member", callback);//7
+  socket.on("update-status", callback);//8
+  socket.on("report-submitted-leader", callback);//9
+  socket.on("report-evaluated-leader", callback);//10
+  socket.on("task-overdue", callback);//11
+  socket.on("project-overdue", callback);//12
+  socket.on("project-status-updated", callback);//13
+  socket.on("member-added", callback);//14
 };
 
 export const disconnectSocket = () => {
